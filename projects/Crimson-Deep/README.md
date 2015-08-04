87790ba0d0fdaeb53aa4445eaceeac49f023b22f
Jasper Holton <jasper@holton.io>
2015-08-04 15:34:54 -0800

    Website fixes, completions, and blog updates.

---
+++ projects/Crimson-Deep/README.md
@@ -0,0 +1,2 @@
+# Crimson-Deep
+My adventures with game making in the form of a tunneling based game project which I call "Crimson Deep"
---
+++ projects/Crimson-Deep/WebServer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Run web server
+
+cd /Users/jasperholton/Programming/Phaser\ Games/Crimson-Deep
+python -m SimpleHTTPServer
+
